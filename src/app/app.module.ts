09ae93faf4f1766cd53ae5def1cf6b254d8d8f53
Nickolas Humeniuk <kolin98111@gmail.com>
2017-12-07 20:04:45 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { GridsterModule } from './gridster/gridster.module';
+
+
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        TestComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        GridsterModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
